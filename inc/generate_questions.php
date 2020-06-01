041115de7fb69b5b67ddf70efe3642297f196db8
strangelights <jbrookshire7@gmail.com>
2020-05-31 23:24:36 -0400

    Add function for random array

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -1,32 +1,44 @@
 <?php
+
 // Generate random questions
 
-$rand_questions = [];
-
-// Loop for required number of questions
- for ($i=0; $i<10; $i++){
-
-    // Get random numbers to add
-    $leftAdder = rand(1,99);
-    $rightAdder = rand(1,99);
-
-    // Calculate correct answer
-    $correctAnswer = $leftAdder + $rightAdder;
+function getRandQuestions($total) {
     
-    // Get unique, incorrect answers within 10 numbers either way of correct answer
-    $firstIncorrectAnswer = $correctAnswer - rand(1,10);
-    $secondIncorrectAnswer = $correctAnswer + rand(1,10);
+    $randQuestions = [];
 
-    // Add question and answer to questions array
-    $rand_questions[] =
-    [
-        "leftAdder" => $leftAdder,
-        "rightAdder" => $rightAdder,
-        "correctAnswer" => $correctAnswer,
-        "firstIncorrectAnswer" => $firstIncorrectAnswer,
-        "secondIncorrectAnswer" => $secondIncorrectAnswer,
-    ];
- }
+    // Loop for required number of questions
+    for ($i=0; $i<$total; $i++){
 
-// var_dump($rand_questions);
+        // Get random numbers to add
+        $leftAdder = rand(1,99);
+        $rightAdder = rand(1,99);
+
+        // Calculate correct answer
+        $correctAnswer = $leftAdder + $rightAdder;
+        
+        // Get unique, incorrect answers within 10 numbers either way of correct answer
+        $firstIncorrectAnswer = $correctAnswer + rand(-10,10);
+        $secondIncorrectAnswer = $correctAnswer + rand(-10,10);
+        
+        // Roll again if answers are duplciated 
+        if ($firstIncorrectAnswer === $secondIncorrectAnswer) {
+            ++$secondIncorrectAnswer;
+        }
+
+        // Add question and answer to questions array
+        $randQuestions[] =
+        [
+            "leftAdder" => $leftAdder,
+            "rightAdder" => $rightAdder,
+            "correctAnswer" => $correctAnswer,
+            "firstIncorrectAnswer" => $firstIncorrectAnswer,
+            "secondIncorrectAnswer" => $secondIncorrectAnswer,
+        ];
+    }
+
+    return $randQuestions;
+
+}
+
+
 
--- inc/quiz.php
+++ inc/quiz.php
@@ -2,17 +2,20 @@
 
 session_start();
 
-include("questions.php");
 include("generate_questions.php");
 
-$total_questions = count($rand_questions);
 $display_score = false;
-$index = null;
-$question = null;
 
-//Check answer submission against answer key and display result via toast message
+// Generate new bank of random questions with each new session, default is 10
+if (!isset($_SESSION["questions"])) {
+    $_SESSION["questions"] = getRandQuestions(10);
+}
+
+$total_questions = count($_SESSION["questions"]);
+
+// Check answer submission against answer key and display result via toast message
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if ($_POST["answer"]== $rand_questions[$_POST["index"]]["correctAnswer"]) {
+    if ($_POST["answer"]== $_SESSION["questions"][$_POST["index"]]["correctAnswer"]) {
         $toast_message = "Congratulations! Your answer is correct.";
         $_SESSION["total_correct"]++;
     } else {
@@ -29,6 +32,7 @@ if (!isset($_SESSION["used_indexes"])) {
 // Display score if all questions have been answered
 if (count($_SESSION["used_indexes"]) === $total_questions) {
     $_SESSION["used_indexes"] = [];
+    unset($_SESSION["questions"]);
     $display_score = true;
 } else {
     $display_score = false;
@@ -39,10 +43,10 @@ if (count($_SESSION["used_indexes"]) === $total_questions) {
 
     // Prevent repeat questions
     do {
-        $index = array_rand($rand_questions);
+        $index = array_rand($_SESSION["questions"]);
     } while (in_array($index,$_SESSION["used_indexes"]));
 
-    $question = $rand_questions[$index];
+    $question = $_SESSION["questions"][$index];
     array_push($_SESSION["used_indexes"], $index);
 
     $answers = [
@@ -54,5 +58,3 @@ if (count($_SESSION["used_indexes"]) === $total_questions) {
     // Randomize order of multiple choice answers
     shuffle($answers);
 }
-
-// session_destroy(); // Debug
